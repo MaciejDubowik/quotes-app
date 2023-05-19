27b3e0802da7d2d98ca0203b6da28538a54c68df
MaciejDubowik <maciejxpoland@gmail.com>
2023-05-19 19:59:21 +0200

    create Quotes screen UI

--- quotes-app/Screens/Quotes/View/QuotesViewController.swift
+++ quotes-app/Screens/Quotes/View/QuotesViewController.swift
@@ -11,9 +11,32 @@ class QuotesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        setupMainView()
+        setupQuoteCard()
+        setupButtonSection()
         setupNavbar()
         setupAutoLayout()
+    }
+    
+    func setupMainView(){
+        view.backgroundColor = UIColor(
+            red: CGFloat(0x2A) / 255.0,
+            green: CGFloat(0x39) / 255.0,
+            blue: CGFloat(0x90) / 255.0,
+            alpha: 1.0
+        )
+    }
+    
+    func setupQuoteCard(){
+        view.addSubview(quoteCardView)
+        quoteCardView.addSubview(quoteLabel)
+        quoteCardView.addSubview(quoteAuthorLabel)
+    }
+    
+    func setupButtonSection(){
+        view.addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(nextButton)
+        buttonStackView.addArrangedSubview(addToFavoritesButton)
     }
     
     func setupNavbar(){
@@ -25,16 +48,100 @@ class QuotesViewController: UIViewController {
     }
     
     func setupAutoLayout() {
+        navbarView.heightAnchor.constraint(equalToConstant: 77).isActive = true
         navbarView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         navbarView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        navbarView.heightAnchor.constraint(equalToConstant: 77).isActive = true
         navbarView.bottomAnchor.constraint(equalTo: view.bottomAnchor ).isActive = true
         
         navbarStackView.leftAnchor.constraint(equalTo: navbarView.leftAnchor).isActive = true
         navbarStackView.rightAnchor.constraint(equalTo: navbarView.rightAnchor).isActive = true
         navbarStackView.heightAnchor.constraint(equalToConstant: 60).isActive = true
-    }
         
+        buttonStackView.bottomAnchor.constraint(equalTo: navbarStackView.topAnchor, constant: -100).isActive = true
+        buttonStackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        buttonStackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+        
+        nextButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        addToFavoritesButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        addToFavoritesButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        
+        quoteCardView.heightAnchor.constraint(equalToConstant: 450).isActive = true
+        quoteCardView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+        quoteCardView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+        quoteCardView.bottomAnchor.constraint(equalTo: buttonStackView.topAnchor,constant: -40).isActive = true
+        
+        quoteLabel.topAnchor.constraint(equalTo: quoteCardView.topAnchor, constant: 40).isActive = true
+        quoteLabel.leftAnchor.constraint(equalTo: quoteCardView.leftAnchor, constant: 20).isActive = true
+        quoteAuthorLabel.topAnchor.constraint(equalTo: quoteCardView.bottomAnchor, constant: -60).isActive = true
+        quoteAuthorLabel.leftAnchor.constraint(equalTo: quoteCardView.leftAnchor, constant: 20).isActive = true
+    }
+    
+    
+    
+    //MARK: - Quote Card
+    
+    private let quoteCardView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 30
+        return view
+    }()
+    
+    private let quoteLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "This is a random quote example."
+        return label
+    }()
+    
+    private let quoteAuthorLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Author Name"
+        return label
+    }()
+    
+    
+    
+    //MARK: - Button section
+    
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.spacing = 30
+        return stackView
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Next", for: .normal)
+        button.backgroundColor = UIColor(
+            red: CGFloat(0xF0) / 255.0,
+            green: CGFloat(0x62) / 255.0,
+            blue: CGFloat(0x92) / 255.0,
+            alpha: 1.0
+        )
+        button.layer.cornerRadius = 40
+        return button
+    }()
+    
+    private let addToFavoritesButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("♡", for: .normal)
+        button.backgroundColor = .white
+        button.tintColor = .black
+        button.layer.cornerRadius = 40
+        return button
+    }()
+    
+    
+    
+    //MARK: - Navbar
+    
     private let navbarView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -70,4 +177,8 @@ class QuotesViewController: UIViewController {
     private let navbarProfileButton: UIButton = {
         return navbarButton(title: "Profile")
     }()
+    
+    
 }
+
+
